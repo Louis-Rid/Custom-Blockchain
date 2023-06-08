48958da7ff8c5d48dd37817c0b04b8f224930e06
Louis-Rid <131224467+Louis-Rid@users.noreply.github.com>
2023-06-08 10:31:54 -0600

    Added signing functionality and began the verification functionaility

--- classes/transaction.js
+++ classes/transaction.js
@@ -1,3 +1,13 @@
+const {
+  keccak256,
+  ecrecover,
+  toChecksumAddress,
+  rlp,
+  ecsign,
+  toRpcSig,
+  fromRpcSig,
+} = require("ethereumjs-util");
+
 class Transaction {
   constructor(to, signature, nonce, value, data, gasLimit, gasPrice) {
     this.to = to;
@@ -7,6 +17,53 @@ class Transaction {
     this.data = data;
     this.gasLimit = gasLimit;
     this.gasPrice = gasPrice;
+  }
+
+  // Signs transaction
+  sign(privateKey) {
+    // Encodes transaction data and creates transaction hash
+    const encodedTransaction = this.encodedTransaction();
+    const transactionHash = keccak256(encodedTransaction);
+
+    // sign transaction with privateKey
+    const { v, r, s } = ecsign(transactionHash, privateKey);
+    //console.log("transaction v,r,s", v, r, s);
+
+    // Convert signature to RPC format
+    const rpcSignature = toRpcSig(v, r, s);
+    //console.log("RPC signature", rpcSignature);
+
+    // Set signature of transaction
+    this.signature = rpcSignature;
+  }
+
+  // Verifies the Signature of the transaction
+  verifySignature() {
+    // Encodes transaction data and creates transaction hash
+    const encodedTransaction = this.encodedTransaction();
+    const transactionHash = keccak256(encodedTransaction);
+    console.log("transaction hash:", transactionHash);
+
+    const { v, r, s } = fromRpcSig(this.signature);
+    console.log("components", v, r, s);
+
+    // Recover PublicKey from signature
+    const publicKey = ecrecover(transactionHash, v, r, s);
+    console.log("recovered public key:", publicKey);
+  }
+
+  // Converts transaction data into a byte array
+  encodedTransaction() {
+    const encodedTransaction = rlp.encode([
+      this.nonce,
+      this.gasPrice,
+      this.gasLimit,
+      this.to,
+      this.value,
+      this.data,
+    ]);
+
+    return encodedTransaction;
   }
 }
 
--- scripts/runChain.js
+++ scripts/runChain.js
@@ -1,10 +1,19 @@
 const Blockchain = require("../classes/blockchain");
 const Block = require("../classes/block");
+const Transaction = require("../classes/transaction");
 
-const blockchain = new Blockchain();
+// const blockchain = new Blockchain();
 
-blockchain.addBlock(new Block(1, { amount: 4 }));
-blockchain.addBlock(new Block(2, { amount: 8 }));
+// blockchain.addBlock(new Block(1, { amount: 4 }));
+// blockchain.addBlock(new Block(2, { amount: 8 }));
 
-console.log("Blockchain is valid:", blockchain.isChainValid());
-console.log(JSON.stringify(blockchain, null, 2));
+// console.log("Blockchain is valid:", blockchain.isChainValid());
+// console.log(JSON.stringify(blockchain, null, 2));
+
+const transaction = new Transaction("123", "123", "1", 1, "123", 1, 1);
+const privateKey = new Uint8Array([
+  10, 20, 30, 40, 50, 60, 70, 80, 10, 20, 30, 40, 50, 60, 70, 80, 10, 20, 30,
+  40, 50, 60, 70, 80, 10, 20, 30, 40, 50, 60, 70, 80,
+]);
+transaction.sign(privateKey);
+transaction.verifySignature();
