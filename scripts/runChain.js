c6bd01a00cc41a81506e45845476357cc046a6d3
Louis-Rid <131224467+Louis-Rid@users.noreply.github.com>
2023-06-12 09:03:57 -0600

    removed console.logs and add secure private key with dotenv

--- scripts/runChain.js
+++ scripts/runChain.js
@@ -1,19 +1,11 @@
+require("dotenv").config();
 const Blockchain = require("../classes/blockchain");
 const Block = require("../classes/block");
 const Transaction = require("../classes/transaction");
-
-// const blockchain = new Blockchain();
-
-// blockchain.addBlock(new Block(1, { amount: 4 }));
-// blockchain.addBlock(new Block(2, { amount: 8 }));
-
-// console.log("Blockchain is valid:", blockchain.isChainValid());
-// console.log(JSON.stringify(blockchain, null, 2));
+const privateKey = process.env.PRIVATE_KEY;
 
 const transaction = new Transaction("123", "123", "1", 1, "123", 1, 1);
-const privateKey = new Uint8Array([
-  10, 20, 30, 40, 50, 60, 70, 80, 10, 20, 30, 40, 50, 60, 70, 80, 10, 20, 30,
-  40, 50, 60, 70, 80, 10, 20, 30, 40, 50, 60, 70, 80,
-]);
-transaction.sign(privateKey);
+const signablePrivateKey = new Uint8Array(Buffer.from(privateKey, "hex"));
+
+transaction.sign(signablePrivateKey);
 transaction.verifySignature();
