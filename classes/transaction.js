8273c5b606dbdfdb952b38f4d743f110089b0c94
Louis-Rid <131224467+Louis-Rid@users.noreply.github.com>
2023-06-12 08:55:57 -0600

    removed console.logs

--- classes/transaction.js
+++ classes/transaction.js
@@ -27,11 +27,9 @@ class Transaction {
 
     // sign transaction with privateKey
     const { v, r, s } = ecsign(transactionHash, privateKey);
-    //console.log("transaction v,r,s", v, r, s);
 
     // Convert signature to RPC format
     const rpcSignature = toRpcSig(v, r, s);
-    //console.log("RPC signature", rpcSignature);
 
     // Set signature of transaction
     this.signature = rpcSignature;
@@ -42,14 +40,11 @@ class Transaction {
     // Encodes transaction data and creates transaction hash
     const encodedTransaction = this.encodedTransaction();
     const transactionHash = keccak256(encodedTransaction);
-    console.log("transaction hash:", transactionHash);
 
     const { v, r, s } = fromRpcSig(this.signature);
-    console.log("components", v, r, s);
 
     // Recover PublicKey from signature
     const publicKey = ecrecover(transactionHash, v, r, s);
-    console.log("recovered public key:", publicKey);
   }
 
   // Converts transaction data into a byte array
