9e6dfdb90073ab55cb70f80563db1af915209a8d
Louis-Rid <131224467+Louis-Rid@users.noreply.github.com>
2023-06-14 09:35:36 -0600

    Added verification of transactions

--- scripts/runChain.js
+++ scripts/runChain.js
@@ -2,10 +2,17 @@ require("dotenv").config();
 const Blockchain = require("../classes/blockchain");
 const Block = require("../classes/block");
 const Transaction = require("../classes/transaction");
-const privateKey = process.env.PRIVATE_KEY;
 
-const transaction = new Transaction("123", "123", "1", 1, "123", 1, 1);
-const signablePrivateKey = new Uint8Array(Buffer.from(privateKey, "hex"));
+function run() {
+  const privateKey = process.env.PRIVATE_KEY;
 
-transaction.sign(signablePrivateKey);
-transaction.verifySignature();
+  try {
+    const transaction = new Transaction("123", privateKey, "1", 1, "123", 1, 1);
+    transaction.sign();
+    transaction.verifySignature();
+  } catch (e) {
+    console.log("Tried to run chain but got: ------:", e);
+  }
+}
+
+run();
